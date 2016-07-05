d9f07660527832af65734967e1086f51c3e0a636
IMMORTALxJO <iblaaaz@gmail.com>
2016-07-05 21:57:41 +0300

    modified:   erlang/primes.erl prime generator

--- erlang/primes.erl
+++ erlang/primes.erl
@@ -1,18 +1,10 @@
+%% Generate primes lower when input arg
+%% primes:gen3(100).
+%% [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
+
 -module(primes).
--export([gen/2,gen2/2]).
+-export([gen/1]).
 
-gen(0,E)-> gen(2,E);
-gen(S,E) ->
-	A=[X||SL<-lists:seq(2,S),EL<-lists:seq(2,E),X<-[SL*EL],X>=S,X=<E],
-	lists:seq(S,E)--A.
-
-%% S - start of window
-%% E - end of window
-%% B - now counter
-%% P - possible primes
-gen2(S,E)->
-	gen2(S,E,2,lists:seq(S,E),trunc(math:sqrt(E)) ).
-gen2(S,E,B,P,T) when B > T -> P;
-gen2(S,E,B,P,T)->
-	TA=[X*B||X<-lists:seq(trunc(S/B),trunc(E/B)),X*B>=S,X*B=<E],
-	gen2(S,E,B+1,P--TA,T).
+gen(S)-> gen(lists:seq(2,S),[]).
+gen([],P) -> lists:reverse(P);
+gen([H|T],P)->	gen( [X||X<-T,X rem H /= 0],[H|P]).
