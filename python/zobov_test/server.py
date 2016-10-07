4644104d080ac73e8188e567f757ad8e88c468f3
iblaaaz <iblaaaz@gmail.com>
2016-10-07 07:29:56 +0000

    "python" dir has been added

---
+++ python/zobov_test/server.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+import socket
+import re
+import json
+from tornado import websocket, web, ioloop, gen, iostream
+from tornado.tcpserver import TCPServer
+
+# vars
+online_socks = []
+online_hosts = []
+
+
+# tcp server
+class smallTcpHandler(object):
+    def __init__(self, stream):
+        self.name = False
+        self.stream = stream
+        self.stream.socket.setsockopt(
+            socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        self.stream.socket.setsockopt(
+            socket.IPPROTO_TCP, socket.SO_KEEPALIVE, 1)
+        self.stream.set_close_callback(self.on_disconnect)
+
+    @gen.coroutine
+    def on_disconnect(self):
+        yield []
+
+    @gen.coroutine
+    def dispatch_client(self):
+        try:
+            while True:
+                line = yield self.stream.read_until(b'\r\n')
+                line = line[:-2]
+                if self.name is False and re.match('^Auth:: [^$]+', line):
+                    self.name = line[7:]
+                    online_hosts.append(self.name)
+                    toAllSocks(json.dumps(['online_hosts', online_hosts]))
+                else:
+                    if not self.name is False:
+                        if line == 'End':
+                            online_hosts.remove(self.name)
+                            self.name = False
+                            toAllSocks(json.dumps(
+                                ['online_hosts', online_hosts]
+                            ))
+                        if re.match('[^\^]+:: [^$]+', line):
+                            toAllSocks(json.dumps(
+                                ['message', self.name, line]
+                            ))
+        except iostream.StreamClosedError:
+            pass
+
+    @gen.coroutine
+    def on_connect(self):
+        raddr = 'closed'
+        try:
+            raddr = '%s:%d' % self.stream.socket.getpeername()
+        except Exception:
+            pass
+        yield self.dispatch_client()
+
+
+class smallTcpServer(TCPServer):
+    @gen.coroutine
+    def handle_stream(self, stream, address):
+        conn = smallTcpHandler(stream)
+        yield conn.on_connect()
+
+tcp_server = smallTcpServer()
+
+
+# web app page + socket server
+def toAllSocks(msg):
+    for sock in online_socks:
+        sock.write_message(msg)
+
+
+class IndexHandler(web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+
+class SocketHandler(websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        if self not in online_socks:
+            online_socks.append(self)
+            self.write_message(json.dumps(['online_hosts', online_hosts]))
+
+    def on_close(self):
+        if self in online_socks:
+            online_socks.remove(self)
+
+
+web_app = web.Application([
+    (r'/', IndexHandler),
+    (r'/ws', SocketHandler)
+])
+
+
+# init servers
+if __name__ == '__main__':
+    tcp_server.listen(8081, '0.0.0.0')
+    web_app.listen(80)
+    ioloop.IOLoop.instance().start()
