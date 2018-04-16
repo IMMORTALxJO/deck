f5ba0d6f7c0829247c9577f8d88c79e86be353ea
Aleksandr Prusov <aprusov@bekitzur.com>
2018-04-16 18:17:29 +0300

    Templinator: I can delete keys and subkeys now

--- python/templinator/templinator.py
+++ python/templinator/templinator.py
@@ -11,11 +11,15 @@ from collections import OrderedDict
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--key', '-k', type=str, required=False,
-          help='A key of dict which will be updated')
+                    help='A key of dict which will be updated')
 parser.add_argument('--state-file', '-sf', type=str, required=True,
-          help='File where state saves')
+                    help='File where state saves')
 parser.add_argument('--template', '-t', type=str, required=False,
-          help='Jinja2 template to render from')
+                    help='Jinja2 template to render from')
+parser.add_argument('--delete', '-d', dest='delete', action='store_true',
+                    required=False, default=False,
+                    help='Delete values or full key from statefile')
+
 (args, another_args) = parser.parse_known_args()
 
 # Parse inserted fields
@@ -23,29 +27,41 @@ parser.add_argument('--template', '-t', type=str, required=False,
 update_data = {}
 data_key = False
 for arg in another_args:
-  if data_key:
-    update_data[data_key] = arg
-    data_key = False
-  elif arg[:2] == '--':
-    data_key = arg[2:]
+    if data_key:
+        update_data[data_key] = arg
+        data_key = False
+    elif arg[:2] == '--':
+        data_key = arg[2:]
+        update_data[data_key] = None
 
 # Read old state, update it and write
 
 try:
-  state_file_fd = open(args.state_file, 'r')
-  state = json.loads(state_file_fd.read(),
-             object_pairs_hook=OrderedDict)
+    state_file_fd = open(args.state_file, 'r')
+    state = json.loads(state_file_fd.read(),
+                       object_pairs_hook=OrderedDict)
 except:
-  state = {}
+    state = {}
 if args.key:
-  state[args.key] = update_data
-  with open(args.state_file, 'w+') as state_file_fd:
-    state_file_fd.write(json.dumps(state, sort_keys=True, indent=2,
-              separators=(',', ': ')))
+
+    if args.delete and args.key in state:
+        if len(update_data.items()) > 0:
+            for sub_key in update_data:
+                state[args.key].pop(sub_key)
+        else:
+            state.pop(args.key)
+
+    if not args.delete:
+        state[args.key] = update_data
+
+    with open(args.state_file, 'w+') as state_file_fd:
+        state_file_fd.write(json.dumps(state, sort_keys=True, indent=2,
+                            separators=(',', ': ')))
 
 # Commit state to template and print render
 
 if args.template:
-  with open(args.template, 'r') as template_file_fd:
-    print Template(template_file_fd.read()).render(state=state)
-  
+    with open(args.template, 'r') as template_file_fd:
+        print Template(template_file_fd.read()).render(state=state)
+
+      
