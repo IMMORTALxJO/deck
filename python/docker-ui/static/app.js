1b669286060e10e6609db9cb8cc22931bf664616
Aleksandr Prusov <aprusov@bekitzur.com>
2018-12-17 15:55:29 +0300

    Docker-ui: Add loading bar

--- python/docker-ui/static/app.js
+++ python/docker-ui/static/app.js
@@ -2,19 +2,23 @@ Vue.use(VueMaterial.default);
 let APP = new Vue({
   el: '#app',
   data: {
-    hosts: {}
+    hosts: {},
+    showLoading: false
   },
   methods: {
     initiate: function(){
       const URL = '/hosts/list';
+      this.showLoading = true;
       this.$http
         .get(URL)
         .then(
           res => {
+            this.showLoading = false;
             res.body.push( location.host );
             res.body.sort( (a,b)=>a.localeCompare(b) ).forEach( hostname=>APP.getContainers(hostname) );
           },
           res => {
+            this.showLoading = false;
             console.error(res);
             alert(`Could not GET ${URL}`);
           },
@@ -24,13 +28,16 @@ let APP = new Vue({
       if( hostname.indexOf(':') < 0 )
         hostname += `:${location.port}`;
       const URL = `http://${hostname}/containers/list`;
+      this.showLoading = true;
       this.$http
         .get(URL)
         .then(
           res => {
+            this.showLoading = false;
             this.$set( this.hosts, hostname, res.body )
           },
           res => {
+            this.showLoading = false;
             console.error(res);
             alert(`Could not GET ${URL}`);
           }
@@ -38,13 +45,16 @@ let APP = new Vue({
     },
     stopContainer: function( hostname, container ){
       const URL = `http://${hostname}/containers/stop/${container.id}`;
+      this.showLoading = true;
       this.$http
         .get(URL)
         .then(
           res => {
+            this.showLoading = false;
             this.$set( this.hosts, hostname, res.body )
           },
           res => {
+            this.showLoading = false;
             console.error(res);
             alert(`Could not GET ${URL}`);
           }
@@ -52,13 +62,16 @@ let APP = new Vue({
     },
     startContainer: function( hostname, container ){
       const URL = `http://${hostname}/containers/start/${container.id}`;
+      this.showLoading = true;
       this.$http
         .get(URL)
         .then(
           res => {
+            this.showLoading = false;
             this.$set( this.hosts, hostname, res.body )
           },
           res => {
+            this.showLoading = false;
             console.log(res);
             alert(`Could not GET ${URL}`);
           }
